d5aa8cf5cdaa47b6443fd7e13628f11303af27b2
erulabs <seandon.mooy@gmail.com>
2019-12-16 14:17:16 -0800

    hot-reload with skaffold

--- src/www/src/App.js
+++ src/www/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          human words <code>src/App.js</code> and save to reload.
+          foobar words <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
